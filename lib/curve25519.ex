62028308e97d47bb85ade59777ae91bc0b3229ab
Matt Miller <mwm@fastmail.com>
2017-09-16 13:52:41 -0400

    Include credo and apply suggestions

--- lib/curve25519.ex
+++ lib/curve25519.ex
@@ -10,8 +10,8 @@ defmodule Curve25519 do
   """
   @type key :: binary
 
-  @p 57896044618658097711785492504343953926634992332820282019728792003956564819949
-  @a 486662
+  @p 57_896_044_618_658_097_711_785_492_504_343_953_926_634_992_332_820_282_019_728_792_003_956_564_819_949
+  @a 486_662
 
   defp clamp(c) do
     c |> band(~~~7)
@@ -21,36 +21,36 @@ defmodule Curve25519 do
 
   defp square(x), do: x * x # :math.pow yields floats.. and we only need this one
 
-  defp expmod(_b,0,_m), do: 1
-  defp expmod(b,e,m) do
-       t = b |> expmod(div(e,2), m) |> square |> rem(m)
-       if (e &&& 1) == 1, do: (t * b) |> rem(m), else: t
+  defp expmod(_b, 0, _m), do: 1
+  defp expmod(b, e, m) do
+       t = b |> expmod(div(e, 2), m) |> square |> rem(m)
+       if (e &&& 1) == 1, do: rem(t * b, m), else: t
   end
 
-  defp inv(x), do: x|> expmod(@p - 2, @p)
+  defp inv(x), do: x |> expmod(@p - 2, @p)
 
-  defp add({xn,zn}, {xm,zm}, {xd,zd}) do
+  defp add({xn, zn}, {xm, zm}, {xd, zd}) do
        x = (xm * xn - zm * zn) |> square |> (&(&1 * 4 * zd)).()
        z = (xm * zn - zm * xn) |> square |> (&(&1 * 4 * xd)).()
-       {rem(x,@p), rem(z,@p)}
+       {rem(x, @p), rem(z, @p)}
   end
-  defp double({xn,zn}) do
+  defp double({xn, zn}) do
        x = (square(xn) - square(zn)) |> square
        z = 4 * xn * zn * (square(xn) + @a * xn * zn + square(zn))
-      {rem(x,@p),  rem(z,@p)}
+      {rem(x, @p),  rem(z, @p)}
   end
 
   defp curve25519(n, base) do
-    one = {base,1}
+    one = {base, 1}
     two = double(one)
-    {{x,z}, _} = nth_mult(n, {one,two})
-    (x * inv(z)) |> rem(@p)
+    {{x, z}, _} = nth_mult(n, {one, two})
+    rem(x * inv(z), @p)
   end
 
   defp nth_mult(1, basepair), do: basepair
-  defp nth_mult(n, {one,two}) do
-     {pm, pm1} = n |> div(2) |> nth_mult({one,two})
-     if (n &&& 1) == 1, do: { add(pm, pm1, one), double(pm1) }, else: { double(pm), add(pm, pm1, one) }
+  defp nth_mult(n, {one, two}) do
+     {pm, pm1} = n |> div(2) |> nth_mult({one, two})
+     if (n &&& 1) == 1, do: {add(pm, pm1, one), double(pm1)}, else: {double(pm), add(pm, pm1, one)}
   end
 
   @doc """
@@ -58,7 +58,7 @@ defmodule Curve25519 do
 
   Returned tuple contains `{random_secret_key, derived_public_key}`
   """
-  @spec generate_key_pair :: {key,key}
+  @spec generate_key_pair :: {key, key}
   def generate_key_pair do
     secret = :crypto.strong_rand_bytes(32) # This algorithm is supposed to be resilient against poor RNG, but use the best we can
     {secret, derive_public_key(secret)}
@@ -70,14 +70,14 @@ defmodule Curve25519 do
   Given our secret key and our partner's public key, returns a
   shared secret which can be derived by the partner in a complementary way.
   """
-  @spec derive_shared_secret(key,key) :: key | :error
+  @spec derive_shared_secret(key, key) :: key | :error
   def derive_shared_secret(our_secret, their_public) when byte_size(our_secret) == 32 and byte_size(their_public) == 32 do
     our_secret |> :binary.decode_unsigned(:little)
                |> clamp
                |> curve25519(:binary.decode_unsigned(their_public, :little))
                |> :binary.encode_unsigned(:little)
   end
-  def derive_shared_secret(_ours,_theirs), do: :error
+  def derive_shared_secret(_ours, _theirs), do: :error
 
   @doc """
   Derive the public key from a secret key
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Curve25519.Mixfile do
 
   def project do
     [app: :curve25519,
-     version: "0.1.4",
+     version: "1.0.1",
      elixir: "~> 1.4",
      name: "Curve25519",
      source_url: "https://github.com/mwmiller/curve25519_ex",
@@ -22,6 +22,7 @@ defmodule Curve25519.Mixfile do
     [
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
+      {:credo, "~> 0.8", only: [:dev, :test]},
     ]
   end
 
